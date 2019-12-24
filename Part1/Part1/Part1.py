0513e833996fc4701da7c42ad7ea52e49ae0e5b3
Ryan-Red <45923095+Ryan-Red@users.noreply.github.com>
2019-12-24 12:06:14 -0500

    Load now works

--- Part1/Part1/Part1.py
+++ Part1/Part1/Part1.py
@@ -1,3 +1,8 @@
 import cv2
 import numpy as np
 
+input_image = cv2.imread("C:/Users/Ryan/source/repos/OpenCVTutorial/images/input.jpg")
+
+cv2.imshow('Hello World', input_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
