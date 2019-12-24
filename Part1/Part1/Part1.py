89ccefbbb528d8dd1343e1b694b1b1b4b33fe8f2
Ryan-Red <45923095+Ryan-Red@users.noreply.github.com>
2019-12-24 12:15:15 -0500

    Now we can write to images too

--- Part1/Part1/Part1.py
+++ Part1/Part1/Part1.py
@@ -1,8 +1,16 @@
 import cv2
 import numpy as np
-
-input_image = cv2.imread("C:/Users/Ryan/source/repos/OpenCVTutorial/images/input.jpg")
+path = "C:/Users/Ryan/source/repos/OpenCVTutorial"
+input_image = cv2.imread(path + "/images/input.jpg")
 
 cv2.imshow('Hello World', input_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#Getting Length and Height
+print("Height of the image is",input_image.shape[0],"pixels")
+print("Length of the image is",input_image.shape[1],"pixels")
+
+#Saving images we modify
+r = cv2.imwrite(path + '/images/output.jpg',input_image)
+print("Status of writting image:",r)
